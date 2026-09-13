40c21748ea37a87e0bb9d68411fc34d0ad4882de
majdidjou704-beep <majdidjou704@gmail.com>
2026-09-14 01:36:37 +0200

    Update Worker.js

--- Worker.js
+++ Worker.js
@@ -21,7 +21,7 @@ export default {
         }
 
         const response = await env.AI.run(
-          "@cf/meta/llama-3.1-8b-instruct",
+          "@cf/meta/llama-3.1-8b-instruct-fast",
           {
             messages: [
               {
